120c70309a49790588b4c564624babaff308f9d4
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-07-15 09:00:12 -0700

    Create mriqc_singleSubj.sh

---
+++ 0-Preprocessing/mriqc/mriqc_singleSubj.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Produces automated QA reports after fmriprep pre-processing
+# Modified script from Freeman Lab (New York University)
+# IRF | Stanford University
+
+# -------- Directory Variables
+SUBJ=$1
+
+SCP="/oak/stanford/groups/jzaki/Social_Networks/project_export/includes/"
+IMAGE="/oak/stanford/groups/jzaki/zaki_images/mriqc-0.15.1.simg"
+OUTPUT="${SCP}/derivatives/mriqc/sub-${SUBJ}"
+FLOATER="/scratch/users/irf823/SCP/work"
+
+mkdir -p $OUTPUT
+
+# -------- Script
+singularity exec --cleanenv $IMAGE              \
+    mriqc $SCP $OUTPUT                          \
+    participant --participant_label $SUBJ       \
+    -w $FLOATER
