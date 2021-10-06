aee987831f0139c74f59d9de5ab3a178f954fbbd
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-10-06 16:03:24 -0700

    Create deploy_socialeval.sh

---
+++ 1-First-Level-GLM/HPC-deployment/deploy_socialeval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=first-level-socialeval.job
+#SBATCH --time=8-00:00
+#SBATCH --mem=12000
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=irf823@stanford.edu
+#SBATCH -c 8
+#SBATCH -N 1
+
+ml python/3.6.1
+source ./level1/bin/activate
+
+python3 firstlevel.py ./includes/ 'socialeval'
