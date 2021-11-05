27f57cd6fac5f72c8532b3034909c19b73b93eaa
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-11-05 14:14:26 -0700

    Create wrapper.py

---
+++ 1-First-Level-GLM/HPC-deployment/wrapper.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+"""
+About this Script
+
+* Instantiates subject object
+* Runs first-level GLM
+
+Ian Richard Ferguson | Stanford University
+"""
+
+# ----- Imports
+from communities import SCP_Sub
+import sys
+
+task_name = sys.argv[1]
+sub_id = sys.argv[2]
+
+sub = SCP_Sub(sub_id, task_name)
+sub.run_first_level_glm()
